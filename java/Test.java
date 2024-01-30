14cae3e99a64890a87a59bd61c7afde1e40f6ec8
Saubhagya <saubhagyabaliarsingh2@gmail.com>
2024-01-30 09:59:37 +0530

    changes

--- java/Test.java
+++ java/Test.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 class Test
 {
 //     public static void main(String args[])
@@ -455,30 +455,30 @@ class Test
 //   System.out.println("The reverse number"+rev);
 // }
 // }
-public static void main(String args[]){
-  // for(int i=1;i<=5;i++){
-  //   for(int j=1;j<=i;j++){
-  //     System.out.print("*");
-  //   }
-  //   System.out.println();
-  // }}
-  // }
-  for(int  i=1;i<=4;i++){
-    for(int j=1;j<=4-i;j++){
-      System.out.print(" ");
-    } 
-    for(int k=1;k<=2*i-1;k++){
-      System.out.print("*");
-    }
-    System.out.println( );
-  }
-  for(int i=4;i>=1;i--){
-    for(int j=1;j<=4-i;j++){
-      System.out.print(" ");
-    }
-    for(int k=1;k<=2*i-1;k++){
-      System.out.print("*");
-    }
-    System.out.println();
-  }}
-  }
+// public static void main(String args[]){
+//   // for(int i=1;i<=5;i++){
+//   //   for(int j=1;j<=i;j++){
+//   //     System.out.print("*");
+//   //   }
+//   //   System.out.println();
+//   // }}
+//   // }
+//   for(int  i=1;i<=4;i++){
+//     for(int j=1;j<=4-i;j++){
+//       System.out.print(" ");
+//     } 
+//     for(int k=1;k<=2*i-1;k++){
+//       System.out.print("*");
+//     }
+//     System.out.println( );
+//   }
+//   for(int i=4;i>=1;i--){
+//     for(int j=1;j<=4-i;j++){
+//       System.out.print(" ");
+//     }
+//     for(int k=1;k<=2*i-1;k++){
+//       System.out.print("*");
+//     }
+//     System.out.println();
+//   }}
+//   }
